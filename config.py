bf3a2b4ae855d9736469198b45de459c9ada11b7
RIFAT1287 <92775750+RIFAT1287@users.noreply.github.com>
2024-01-16 21:27:26 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@
 # TOKEN = '4213543775:AsfjgfgdsdhfsdjhgjhfgsjgY'
 
 
-API_ID = ''
-API_HASH = ''
-TOKEN = ''
+API_ID = '11421014'
+API_HASH = '9ac3fd02dccbde3435d6b58a5e430bcb'
+TOKEN = '6703336293:AAEgB68KnrnEUUif273nNYQ3sgLhdwor-j0'
